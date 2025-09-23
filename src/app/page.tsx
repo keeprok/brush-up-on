84e6f71ffa3ebe3d32061ba6405189fcb53f6d20
keeprok <140785214+keeprok@users.noreply.github.com>
2025-09-23 16:31:22 +0900

    dd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import InfityScrollPage from '@/components/3/CssAnimation/1/InfityScrollPage';
 import IsModalHiddenPage from '@/components/5/Cookie/question/IsModalHiddenPage';
 
 export default function Home() {
+  console.log(1);
   return (
     <div>
       <InfityScrollPage />
